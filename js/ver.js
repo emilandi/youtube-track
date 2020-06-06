173be97f292027286692aa18bf0b4bd050457725
emilandi <emilandi2000@hotmail.com>
2020-06-06 11:25:10 -0300

    update

--- js/ver.js
+++ js/ver.js
@@ -21,30 +21,41 @@ function init(){
 	}, 2000);
 	
 	console.clear();
-	console.log('ready!');
+	console.log('ready!');	
+	var h1 = document.querySelector('h1');
+	var video = document.querySelector('video');
+	var desc = document.querySelector('#description');
 
-	//inject script file
-	var s = document.createElement('script');
-	// TODO: add "script.js" to web_accessible_resources in manifest.json
-	s.src = chrome.runtime.getURL('js/inject.js');
-	s.onload = function() {
-    	this.remove();
-	};
-	(document.head || document.documentElement).appendChild(s);
+	console.log(h1,video,desc);
 
-	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {		  
-		if (request.action){
-			console.log(request);
-		}			
-	});
+	document.addEventListener('mousemove',createDiv);
+	
 
+
+}
+
+function createElement(tipo,id,clase,text) {
+	
+	var obj = document.createElement(tipo);	
+	
+	if(id){
+		obj.id=id;
+	}
+	
+	if(clase){
+		obj.classList.add(clase);		
+	}
+
+	obj.innerText=text;
+	
+	return obj;	
 }
 
 function fnVideo () {	
 	var clase='video-stream html5-main-video';			
 
 	video = document.getElementsByClassName(clase)[0];
-	video = document.querySelector('video');	
+	video = document.querySelector('video');
 
 	if(video){
 		
@@ -67,76 +78,100 @@ function fnVideo () {
 
 
 function createDiv () {
+	console.log('Creando Div');
+	var desc =  document.getElementById('description');
+		
+	if(desc){
+		
+		//crear div + buttons								
+
+		var objClass = desc.getElementsByClassName(clase);
+		var objSelector = desc.querySelectorAll(selector);	
+
+		var h1 = 'h1.title.style-scope.ytd-video-primary-info-renderer';					
+		var root = document.querySelector(h1);				  
+		var elem = document.getElementById('track');
+		
+		if(!elem){			
+			
+			var div = createElement('div','track','','');
+			var btnAdelante = createElement('a','next','ytClass','>>');	
+			var btnAtras = createElement('a','back','ytClass','<<');
+			div.appendChild(btnAtras);							
+			div.appendChild(btnAdelante);
+
+			root.appendChild(div);					
 	
-	var host=getHost();
+			btnAtras.addEventListener('click',fnAtras);
+			btnAdelante.addEventListener('click',fnAdelante);
+		
+			btnAtras.addEventListener('mouseover',function(e){										
+				getPos();					
+				var title='';
+				var actual = trackActual(video.currentTime);					
+				if(actual>1){												
+					var track = parseFloat(actual-1);						
+					var info = 'Track N° ' + track ;
+				}else{
+					var info = 'Track N° 1';
+				}	
+
+				this.setAttribute('title',info);
+				
+				console.clear();
+				console.log(video.currentTime + ' -  Current track: ' + actual);					
+			})
+			
+			btnAdelante.addEventListener('mouseover',function(e){					
+				getPos();					
+				var title = '';					
+				var actual = trackActual(video.currentTime);
+				if(actual < pos.length){					
+					var track = parseFloat(actual+1);
+					var info = 'Track N° ' + track ;						
+				}					
+				this.setAttribute('title',info);										
+				
+				console.clear();
+				console.log(video.currentTime + ' -  Current track: ' + actual);					
+
+			})			
+		
+			createInfoTool(); //crear barra de informacion		
+		}						
+		
+	}else{
+		console.log('No Hay descripcion');
+	}		
+	$('track').fadeIn(600);
+	// fnShow('track','block');
+		
+}
+
+
+function createInfoTool() {
+	var root = document.querySelector('.html5-video-container');
 	
-		var desc =  document.getElementById('description');
-		if(desc){			
+	if(root){
+		var div = document.getElementById('infobar')
+		if(!div){
+			div = createElement('div','infobar','','');
+			root.appendChild(div);
+		}
+	}
+}
 
-			var objClass = desc.getElementsByClassName(clase);
-			var objSelector = desc.querySelectorAll(selector);	
+function showMsj(msj) {
+	var elem = document.getElementById('infobar');
+	if(elem){
+		elem.textContent=msj;
+		elem.style.display='block';
 
-			var H1 = 'h1.title.style-scope.ytd-video-primary-info-renderer';
-
-			var root = document.querySelector(H1);			
-			var elem = document.getElementById('track');
-			
-			if(!elem){
-				
-				var div = document.createElement('div');
-				div.id='track';
-				div.style.display='none';
-				root.appendChild(div);			
+		setTimeout(function(){
+			$('#infobar').fadeOut(300);
+		}, 3000);
 		
-				var btnAtras = createBtn('a','back','ytClass','<<');		
-				var btnAdelante = createBtn('a','next','ytClass','>>');	
-		
-				btnAtras.addEventListener('click',fnAtras);
-				btnAdelante.addEventListener('click',fnAdelante);
-			
-				btnAtras.addEventListener('mouseover',function(e){										
-					getPos();
-					//getNombresNew();					
-					var title='';
-					var actual = trackActual(video.currentTime);					
-					if(actual>1){						
-						//var title = nombres[actual-1];
-						var track = parseFloat(actual-1);						
-						var info = 'Track N° ' + track ;
-					}else{
-						var info = 'Track N° 1';
-					}	
-
-					this.setAttribute('title',info);
-					
-					console.clear();
-					console.log(video.currentTime + ' -  Current track: ' + actual);					
-				})
-				
-				btnAdelante.addEventListener('mouseover',function(e){					
-					getPos();
-					//getNombresNew();
-					var title = '';					
-					var actual = trackActual(video.currentTime);
-					if(actual < pos.length){
-						//var title = nombres[actual+1];
-						var track = parseFloat(actual+1);
-						var info = 'Track N° ' + track ;						
-					}					
-					this.setAttribute('title',info);										
-					
-					console.clear();
-					console.log(video.currentTime + ' -  Current track: ' + actual);					
-
-				})			
-			}						
-			
-		}else{
-			console.log('No Hay descripcion');
-		}		
-		
-		fnShow('track','block');
-		
+	}
 }
 
 function checkPos(){	
@@ -145,7 +180,9 @@ function checkPos(){
 	if(len==0){			
 		setColor('back',disableColor);
 		setColor('next',disableColor);
+		//$('.ytClass').fadeOut(300);
 	}else{	
+		$('.ytClass').fadeIn(300);
 		setColor('back',enableColor);
 		setColor('next',enableColor);	
 	
@@ -161,6 +198,11 @@ function checkPos(){
 	}
 
 	return pos.length;
+}
+
+
+function setClass(value) {
+	document.querySelector(value).setClass(value);
 }
 
 function setColor(id,value) {
@@ -193,24 +235,24 @@ function fillPos(value){
 	}	
 }
 
-function fnShow(value){	
+// function fnShow(value){	
 
-	//sino tiene tracks ocultar DIV
-	if(value=='none'){		
-		$("#track").fadeIn(600, function () {					
-			$("#back").show().fadeOut(600);
-			$("#next").show().fadeOut(600);			
-		});	
-	}else{
-		$("#track").fadeIn(600, function () {					
-			$("#back").show().fadeIn(600);
-			$("#next").show().fadeIn(600);			
-		});	
-	}		
-}
+// 	//sino tiene tracks ocultar DIV
+// 	if(value=='none'){		
+// 		$("#track").fadeIn(600, function () {					
+// 			$("#back").show().fadeOut(600);
+// 			$("#next").show().fadeOut(600);			
+// 		});	
+// 	}else{
+// 		$("#track").fadeIn(600, function () {					
+// 			$("#back").show().fadeIn(600);
+// 			$("#next").show().fadeIn(600);			
+// 		});	
+// 	}		
+// }
 
 function fnAtras(){
-	
+	var ntrack = 1;
 	var time = video.currentTime;		
 	var nro = dameSalto(time,'back');
 	var actual=trackActual(time);
@@ -224,19 +266,60 @@ function fnAtras(){
 		if(time > pos[0]  && time < pos[1] ){
 			nro=pos[0];
 		}
+	}	
+	
+	if(actual>1){
+		ntrack = actual - 1;
 	}
+	var msj = "Track N° " + ntrack;
+	
+	//toast(msj);	
+	//$("#back").notify(msj,{ position:"right" },"success" );
+	showMsj(msj);
 	
 	playVideo(nro);	
 }
 
-function fnAdelante(){			
+function fnAdelante(){	
 
 	 var time = video.currentTime;	
 	 var nro = dameSalto(time,'next');				
 	 var actual = trackActual(time);	
-	 console.log('Click Adelante - ','Track: '+ actual + ' Tiempo: ' + time +   ' -  Proximo: '  +  nro);			
-	
+	 var msj = "Track N° " + parseFloat(actual + 1);
+	 console.log('Click Adelante - ','Track: '+ actual + ' Tiempo: ' + time +   ' -  Proximo: '  +  nro);	
+	 
+	 //toast(msj);
+	 //$('h1').notify(msj,{ position:"right" },"success" );
+	 showMsj(msj);
 	 playVideo(nro);	
+}
+
+
+
+function toast(msj){
+	
+	//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js
+	//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css	
+	
+	toastr["success"]("Track N° " + msj);
+
+	toastr.options = {
+	  "closeButton": false,
+	  "debug": false,
+	  "newestOnTop": false,
+	  "progressBar": false,
+	  "positionClass": "toast-top-center",
+	  "preventDuplicates": false,
+	  "onclick": null,
+	  "showDuration": "200",
+	  "hideDuration": "100",
+	  "timeOut": "2000",
+	  "extendedTimeOut": "1000",
+	  "showEasing": "swing",
+	  "hideEasing": "linear",
+	  "showMethod": "fadeIn",
+	  "hideMethod": "fadeOut"
+	}	
 }
 
 function getMax() {
@@ -309,21 +392,21 @@ function convert(elem){
 	return 	nro;
 }
 
-function createBtn(tipo,id,clase,texto) {	
+// function createBtn(tipo,id,clase,texto) {	
 	
-	var div = document.getElementById('track');		
+// 	var div = document.getElementById('track');		
 	
-	if (div){		
-		var btn = document.createElement(tipo);	
-		btn.classList.add(clase);
-		btn.id=id;
-		btn.innerText=texto;
-		div.appendChild(btn);
-	}else{
-		console.log('No existe DIV');
-	}	
-	return btn;	
-}
+// 	if (div){		
+// 		var btn = document.createElement(tipo);	
+// 		btn.classList.add(clase);
+// 		btn.id=id;
+// 		btn.innerText=texto;
+// 		div.appendChild(btn);
+// 	}else{
+// 		console.log('No existe DIV');
+// 	}	
+// 	return btn;	
+// }
 
 function getTime () {
 	return video.currentTime;
@@ -333,15 +416,25 @@ function getHost(){
 	return location.hostname;
 }
 
-function getNombres() {	
-	nombres=[];
-	var elem = document.querySelectorAll('#description > yt-formatted-string > span');
+function getNombres(nro) {	
+	// nombres=[];
+	// var elem = document.querySelectorAll('#description > yt-formatted-string > span');
 	
-	elem.forEach(e => {		
-		var fixNombres = fixStr(e.textContent);		
-		nombres.push(fixNombres);
-	});		
-	console.log(nombres);
+	// elem.forEach(e => {		
+	// 	var fixNombres = fixStr(e.textContent);		
+	// 	nombres.push(fixNombres);
+	// });		
+	// console.log(nombres);	
+
+	var nro = 2;
+	var tags = document.querySelector('#scriptTag').innerHTML;
+	var init = tags.indexOf('\\n' + nro);
+	var final = tags.indexOf('\\n' + parseFloat(nro + 1));
+	var nombre = tags.substring(init,final)
+	console.log(init,final,nombre)
+	return	nombre;
+	
+
 }
 
 function fixStr(value) {			
